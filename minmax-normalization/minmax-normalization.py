ee42704951c7708dc0307a4c8b7b46310a702a27
RichKidd <sergioalejandrotr17@gmail.com>
2026-08-24 08:33:18 -0600

    Solve minmax-normalization

--- minmax-normalization/minmax-normalization.py
+++ minmax-normalization/minmax-normalization.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def minmax_scale(X, axis=0, eps = 1e-12):
     X = np.asarray(X, dtype=float)
-    minimo = np.min(X, axis=axis, keepdims=True)
+    minimo = np.min(X, axis=axis, keepdims=True)#dimension (1,2) 1fila, 2 columnas
     maximo = np.max(X, axis=axis, keepdims=True)
     rango = (maximo - minimo) + eps
     return (X - minimo) / rango
